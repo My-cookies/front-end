473c642f5a472d1063c8747d88fe97487ebb5e27
Mael <rgdmael@gmail.com>
2021-10-20 00:22:15 +0200

    feat: modif catalog and add axios

--- src/components/cards/BasicCard.js
+++ src/components/cards/BasicCard.js
@@ -1,19 +1,38 @@
-import "../../assets/css/BasicCard.css"
+import "../../assets/css/BasicCard.css";
+import axios from "axios";
+import toast, { Toaster } from 'react-hot-toast';
 
-const BasicCard = ({nom, prix, image, status}) => {
+const BasicCard = ({nom, prix, image, token}) => {
+    const buyNft = (e) => {
+        e.preventDefault();
+
+        const data = {
+            itemID: token,
+            userID: "741279557839093803",
+        }
+        axios.post("http://localhost:3000/api/shop/buy", data)
+            .then(
+                toast('Le NFT a été mint', {icon: '✔️'})
+            )
+            .catch((error) => {
+                toast('Une erreur est survenue', {icon: '❌'})
+            })
+
+
+    }
+
     return (
-
-            <div id="rectangle">
-                <img className="image" src={image}/>
-                <div className="rectangle-contenu">
-                    <h1 id="nom">{nom}</h1>
-                    <h1 id="prix">{prix}</h1>
-                    <h1 id="status">{status}</h1>
-                    <input type="button" className="button" value="acheter"/>
-
-                </div>
-
+        <div className="rectangle" >
+            <Toaster />
+            <img className="image" src={image}/>
+            <div className="item-contenu">
+                <p id="nom">{nom}</p>
+                <p id="prix">{prix}</p>
+                <input type="button" className="button-buy" value="Mint" onClick={buyNft}/>
             </div>
+
+
+        </div>
 
     )
 }
